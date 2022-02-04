f011c0a905e6cab548de8bb68aebd36d67a0941f
Anthony <andro125@ulaval.ca>
2022-02-04 16:45:26 -0500

    ajout commentaire

--- calculs.py
+++ calculs.py
@@ -7,3 +7,6 @@ def calculs(appareil, type, range, valeur):
     range_mesure = type_mesure[range]
     incertitude = float(valeur) * (float(range_mesure[1])/100) + float(range_mesure[0]) * (float(range_mesure[2])/100)
     return incertitude 
+
+
+# Ceci est un test
