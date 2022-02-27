08c8f4ca71a205a08b7ae311ffa650cdc145bf2f
Laurent <laemo10@ulaval.ca>
2022-02-27 11:13:44 -0500

    ac

--- calculs_intelligent.py
+++ calculs_intelligent.py
@@ -5,6 +5,7 @@ class Incertitude_intelligente:
 
     def __init__(self):
         self.appareil_calcul = ''
+        self.courant = ''
 
         self.window = Tk()
         self.window.title('Incertitude')
@@ -21,12 +22,14 @@ class Incertitude_intelligente:
         self.appareil.pack(expand=True)
 
         self.curent_type = Frame()
-        self.appareil_text = Label(self.window,font=('arial', 20),text="Entrez le type de courant")
-        self.appareil_text.pack(expand=True)
-        self.appareil1_button = Button(self.appareil, text='DC' ,command= lambda : self.type_courant('DC'))
-        self.appareil1_button.grid(row=0,column=0)
-        self.appareil2_button = Button(self.appareil, text='AC',command= lambda : self.type_courant('AC'))
-        self.appareil2_button.grid(row=0,column=1)
+        self.curent = Frame(self.curent_type)
+        self.curent_text = Label(self.window,font=('arial', 20),text="Entrez le type de courant")
+        self.curent_text.pack(expand=True)
+        self.DC_button = Button(self.curent, text='DC' ,command= lambda : self.type_courant('DC'))
+        self.DC_button.grid(row=0,column=0)
+        self.AC_button = Button(self.curent, text='AC',command= lambda : self.type_courant('AC'))
+        self.AC_button.grid(row=0,column=1)
+        self.curent.pack(expand = True)
         self.curent_type.pack(expand=True)
 
         self.Valeur = Frame()
@@ -66,19 +69,43 @@ class Incertitude_intelligente:
             self.appareil3_button.config(foreground="red")
 
     def type_courant(self, value):
-        self.appareil_calcul = float(self.appareil_calcul)+1
+        if value == "AC":
+            self.appareil_calcul = float(self.appareil_calcul)+1
+            self.courant = "AC"
+            self.frequence = Frame(self.curent_type)
+            self.courant_text = Label(self.frequence,font=('arial', 20), text="Entrez votre fréquence")
+            self.courant_text.grid(row=1, column=0)
+            self.courant_entry = Entry(self.frequence, bd=0)
+            self.courant_entry.grid(row=1, column=1)
+            self.frequence.pack(expand=True)
+            self.AC_button.config(foreground="red")
+            self.DC_button.config(foreground="black")
+        elif value == "DC":
+            self.courant = "DC"
+            self.DC_button.config(foreground="red")
+            self.AC_button.config(foreground="black")
 
     def appeler_calculs(self, valeur):
-        print(str(valeur),self.appareil_calcul)
-        try:
-            a = I_A.calcul_intelligent(str(valeur),self.appareil_calcul)
-        except Exception:
-            rep = Tk()
-            rep_text = Label(rep,font=('arial', 20), text='Erreur')
-            rep_text.pack(expand=True)
-            rep.mainloop()
-        self.réponse_text.config(text=f"Votre réponse est : {a[0]}")
-        self.range_text.config(text=f"Le range déterminé est : {a[1]}")
+        if self.courant == "DC":
+            try:
+                a = I_A.calcul_intelligent(str(valeur),self.appareil_calcul)
+            except Exception:
+                rep = Tk()
+                rep_text = Label(rep,font=('arial', 20), text='Erreur')
+                rep_text.pack(expand=True)
+                rep.mainloop()
+            self.réponse_text.config(text=f"Votre réponse est : {a[0]}")
+            self.range_text.config(text=f"Le range déterminé est : {a[1]}")
+        else:
+            try:
+                a = I_A.calcul_intelligent_AC(str(valeur),self.appareil_calcul, self.frequence)
+            except Exception:
+                rep = Tk()
+                rep_text = Label(rep,font=('arial', 20), text='Erreur')
+                rep_text.pack(expand=True)
+                rep.mainloop()
+                self.réponse_text.config(text=f"Votre réponse est : {a[0]}")
+                self.range_text.config(text=f"Le range déterminé est : {a[1]}")
 
     def reset(self):
         self.appareil_calcul = ''
